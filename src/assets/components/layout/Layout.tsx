54715225e75315bf3aae9d0d4369a44baa4d6947
onetwothird <angelitodecatoriaa@gmail.com>
2026-06-21 15:48:45 +0800

    asdded

--- src/assets/components/layout/Layout.tsx
+++ src/assets/components/layout/Layout.tsx
@@ -29,7 +29,7 @@ export default function Layout({ children, toc }: LayoutProps) {
 
             {/* Right Table of Contents */}
             {toc && (
-              <div className="hidden xl:block xl:w-64 xl:shrink-0 xl:-mt-32 relative z-50">
+              <div className="hidden xl:block xl:w-64 xl:shrink-0">
                 <RightTOC items={toc} />
               </div>
             )}
